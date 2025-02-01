0301cf41fccb339be2363de2153582b3e2192d71
Jovan J. <j.j0v4n@pm.me>
2025-02-01 13:16:16 +0000

    fix: fixed the games_played and best_game stat recording;

--- number_guess.sh
+++ number_guess.sh
@@ -22,8 +22,15 @@ then
         echo "It's higher than that, guess again:"
     else
         echo "You guessed it in $tries tries. The secret number was $random_number. Nice job!"
-        record_games_played=$($PSQL "UPDATE number_guess SET games_played = games_played + 1 WHERE username = '$1';")
-        record_best_game=$($PSQL "UPDATE number_guess SET best_game = $tries WHERE username = '$1' AND best_game;")    
+            record_games_played=$($PSQL "UPDATE number_guess SET games_played = games_played + 1 WHERE username = '$1';")
+                best_game=$($PSQL "SELECT best_game FROM number_guess WHERE username='$1';")
+                if [[ $best_game == 0 ]]
+                then
+                record_best_game=$($PSQL "UPDATE number_guess SET best_game = $tries WHERE username = '$1';")
+                elif [[ $tries -lt $best_game ]]
+                then
+                record_best_game=$($PSQL "UPDATE number_guess SET best_game = $tries WHERE username = '$1';")
+                fi
     fi
 else
     echo "That is not an integer, guess again:"
@@ -42,8 +49,8 @@ echo "Welcome, $username! It looks like this is your first time here."
 guessing_game $username
 
 else
-get_games_played=$($PSQL "SELECT games_played FROM number_guess WHERE username='$username';")
-get_best_game=$($PSQL "SELECT best_game FROM number_guess WHERE username='$username';")
-echo "Welcome back, $username! You have played $get_games_played games, and your best game took $get_best_game guesses."
+games_played=$($PSQL "SELECT games_played FROM number_guess WHERE username='$username';")
+best_game=$($PSQL "SELECT best_game FROM number_guess WHERE username='$username';")
+echo "Welcome back, $username! You have played $games_played games, and your best game took $best_game guesses."
 guessing_game $username
 fi
