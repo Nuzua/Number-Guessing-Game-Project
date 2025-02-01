59e0cd9f1f4707aa547fd08bc2e35452aff96504
Jovan J. <j.j0v4n@pm.me>
2025-02-01 18:14:02 +0000

    Submitting my solution for the Number Guessing Game Project.

--- number_guess.sql
+++ number_guess.sql
@@ -60,7 +60,7 @@ ALTER TABLE public.number_guess OWNER TO freecodecamp;
 -- Data for Name: number_guess; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-INSERT INTO public.number_guess VALUES ('Nuzua', 3, 11);
+INSERT INTO public.number_guess VALUES ('Nuzua', 4, 11);
 
 
 --
